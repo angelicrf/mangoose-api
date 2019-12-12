cfb26fcea00c95a40266f943f23e54e9f6f2b111
Your Name <angelicrf@gmail.com>
2019-12-12 14:37:31 -0800

    AppCreated

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://angelref:12bizbiz@angelcuster-vvmjt.mongodb.net/test?retryWrites=true&w=majority'
+};
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require ('mongoose');
+const bodyParser = require('body-parser');
+const items = require('./routes/api/items.js');
+
+const app = express();
+
+app.use(bodyParser.json());
+const db = require('./config/keys.js').mongoURI;
+console.log("db is:",db);
+mongoose.Promise = global.Promise;
+
+const connectDB = async () => {
+    mongoose.connect(db, {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true})
+        .then(() => console.log('MongoDb is Connected..'))
+        .catch(err => console.log(err));
+};
+connectDB();
+app.use('/api/items', items);
+
+const port = process.env.PORT || 30025;
+
+app.listen(port, () => console.log(`Server is running on port: ${port}`));
