546d26d7b7d3caa0f72c3dc58fb065b8af1e3269
Your Name <angelicrf@gmail.com>
2019-12-15 12:59:31 -0800

    WItemsAdd-Dl

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AppNavbar from "./componenets/AppNavbar";
+import ShoppingList from "./ShoppingList";
 function App() {
   return (
     <div className="App">
       <AppNavbar />
+      <ShoppingList />
     </div>
   );
 }
