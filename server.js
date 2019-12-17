66c2c396874490ce79734d7d9197e83ccb76529a
Your Name <angelicrf@gmail.com>
2019-12-17 09:15:35 -0800

    WDoneShoppingList

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require ('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 const items = require('./routes/api/items.js');
 
 const app = express();
@@ -17,6 +18,13 @@ const connectDB = async () => {
 };
 connectDB();
 app.use('/api/items', items);
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 const port = process.env.SERVER_PORT || 30026;
 
