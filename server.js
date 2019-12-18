f1a660c9b0916e990c40b872e8a5272c80778bc3
Your Name <angelicrf@gmail.com>
2019-12-18 12:41:19 -0800

    WCMD

--- server.js
+++ server.js
@@ -19,17 +19,17 @@ const connectDB = async () => {
 };
 connectDB();
 app.get('/', function (req, res){
-    res.sendFile(path.resolve(__dirname,'build/index.html'));
+    res.sendFile(path.join(__dirname +'/build/index.html'));
 });
 app.use('/api/items', items);
 console.log("the process env NODE_ENV", process.env.NODE_ENV);
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('build'));
-    app.get('*', (req,res) => {
-        res.sendFile(path.resolve(__dirname, 'build/index.html'));
+//if(process.env.NODE_ENV === 'production'){
+app.use(express.static('build'));
+app.get('*', (req,res) => {
+        res.sendFile(path.join(__dirname +'/build/index.html'));
     });
-}
+//}
 
 const port = process.env.SERVER_PORT || 30026;
 
